109e21bd836c39b5995dad913d7ab153fd6d200a
Predator <evanslance100@gmail.com>
2025-02-23 23:15:00 +0300

    fix early return for else statement

--- Beginnner Level Algorithms/Linear Search/linear_search.py
+++ Beginnner Level Algorithms/Linear Search/linear_search.py
@@ -1,20 +1,20 @@
 from typing import List
 class Solution:
-    def linearsearch(self, nums: List[int], target: int):
+    def linearsearch(self, nums: List[int], target: int) -> int:
         """
         Perfom a linear search on the list nums to find target 
         """
         for i in range(len(nums)):
             if nums[i] == target:
                 return i
-            else:
-                return -1
+        else:
+            return -1
 
 #impementation example
 if __name__ == "__main__":
     solution = Solution()
     
     nums = [1,2,3,4,5,6,7,0]
-    target = 0
-    result  = solution.linearsearch(nums, 7)
+    target = 2
+    result  = solution.linearsearch(nums, target)
     print(result)
