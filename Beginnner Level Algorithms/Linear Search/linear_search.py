c6a6bb9e4197b038a4c2760810cc7f83b98fa422
Predator <evanslance100@gmail.com>
2025-02-23 23:02:30 +0300

    linear search algorithm

---
+++ Beginnner Level Algorithms/Linear Search/linear_search.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def linearsearch(self, nums: List[int], target: int):
+        """
+        Perfom a linear search on the list nums to find target 
+        """
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            else:
+                return -1
+
+#impementation example
+if __name__ == "__main__":
+    solution = Solution()
+    
+    nums = [1,2,3,4,5,6,7,0]
+    target = 0
+    result  = solution.linearsearch(nums, 7)
+    print(result)
