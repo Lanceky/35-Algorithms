06347934960eff253c185e32f8c703462d8c7501
Predator <evanslance100@gmail.com>
2025-02-24 01:10:54 +0300

    buble sort algorithm

--- Beginnner Level Algorithms/Bubble_sort/bubble.py
+++ Beginnner Level Algorithms/Bubble_sort/bubble.py
@@ -1,11 +1,24 @@
 import random
-from typing import List
-
+import math
+#generating a random list
 aList = []
 for i in range(5):
-    aList.append(random.randrange(1, 50))
+    aList.append(random.randrange(1,50))
 
-print(f"unsorted list: {aList}")
-
+print(f"unordered list : {aList} ")
+print()
 #bubble sort
-iterations
+iterations = len(aList) -  1
+while iterations > 1:
+    j=0
+    while j < iterations:
+        if aList[j] > aList[j+1]:
+            #swap them
+            temp = aList[j]
+            aList[j] = aList[j+1]
+            aList[j+1] = temp
+        j += 1
+    iterations -= 1
+
+for num in aList:
+    print(num, end=",")
